fbf60094673e92e600dab05b2719df12f5044f2a
17track <17track.for.test@gmail.com>
2016-03-09 15:54:09 +0800

    补充遗漏文件

---
+++ TestApp/JSPatch/JSPatchDemo_Second.js
@@ -0,0 +1,18 @@
+defineClass('JSPatchDemoViewController', {
+
+//viewDidLoad: function() {
+//        
+//self.super().viewDidLoad();
+//var view = self.view().subviews().firstObject();
+//var gestureRecognizer = require('UITapGestureRecognizer').alloc().initWithTarget_action(self, "tap");
+//
+// view.addGestureRecognizer(gestureRecognizer);
+// view.setUserInteractionEnabled(YES);
+//},
+
+
+tap: function() {
+self.navigationController().popViewControllerAnimated(YES);
+            console.log("重复了..怎么办");
+},
+});
